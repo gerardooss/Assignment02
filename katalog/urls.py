23c30c60058595a23f75ffb4847783de9da6961a
marcelinus.gerardo <marcelinus.gerardo!ui.ac.id>
2022-09-13 22:54:43 +0700

    implement basic views

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,9 @@
 # TODO: Implement Routings Here
+from django.urls import path
+from katalog.views import show_catalog
+
+app_name = 'katalog'
+
+urlpatterns = [
+    path('details/', show_catalog, name='show_catalog'),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(request):
+    katalogTerdaftar = CatalogItem.objects.all()
+    context = {
+    'list_barang': katalogTerdaftar,
+    'nama': 'Marcelinus Gerardo',
+    'npm' : '2106708324'
+    }
+    return render(request, "katalog.html", context)
